5a0d4d028071272dd38811e12fbc768b15fa165f
Mohammed Shafwat Jamil <msj2340@gmail.com>
2017-10-25 19:54:54 -0400

    added new methods to SuperArrays class

--- SuperArray/Driver.java
+++ SuperArray/Driver.java
@@ -1,23 +1,23 @@
 public class Driver{
     public static void main(String[] args){
-	SuperArray suparr= new SuperArray();
-	//Phase 1
-	//Does it print
-	System.out.println(suparr);//expecting []
-	//add stuff to string
-	//System.out.println(suparr.add("death"));//expecting true
-	//System.out.println(suparr.add("pestilence"));//expecting true
-	//System.out.println(suparr);//expecting [death,pestilence]
-	//now clear it
-	//System.out.println(suparr.clear());
-	//System.out.println(suparr);//[]
-	//now resize it
-	//System.out.println(suparr.size(11));//11
-	// is it really resized?????
-	//System.out.println(suparr.set(10,"hi"));//0
-	//System.out.println(suparr);//[0,0,0,0,0,0,0,0,0,0,"hi"]
-	//since we set a value lets get it
-	//System.out.println(suparr.get(10));//"hi"	
+      SuperArray suparr= new SuperArray();
+      //Phase 1
+      //Does it print
+      System.out.println(suparr);//expecting []
+      //now find the size
+      System.out.println(suparr.size());//10
+      //add stuff to array
+      System.out.println(suparr.add("death"));//expecting true
+      System.out.println(suparr.add("pestilence"));//expecting true
+      System.out.println(suparr);//expecting [death,pestilence]
+      System.out.println(suparr.size());
+      //now clear it
+      suparr.clear();
+      System.out.println(suparr);//[]
+      System.out.println(suparr.set(8,"hi"));//0
+      System.out.println(suparr);//["hi"]
+      //since we set a value lets get it
+      System.out.println(suparr.get(8));//"hi"	
     }
 
 }
--- SuperArray/SuperArray.java
+++ SuperArray/SuperArray.java
@@ -1,20 +1,97 @@
 public class SuperArray{
-    private String[] data;
-    private int size;
-    public SuperArray(){
-	size=10;
-	data=new String[size];
-    }
-    public String toString(){
-	String printable="[";
-	for(int i=0;i<data.length;i+=1){
+  private String[] data;
+  private int size;
+  public SuperArray(){
+      size=10;
+      data=new String[size];
+   }
+  /**
+     Returns the data in the format: "[A, B, FISH, cat, Dog]" Without double quotes around the data elements.
+     @return A SuperArray as a String
+   */
+
+  public String toString(){
+    int nonNulls=0;
+    String printable="[";
+    for(int i=0;i<data.length;i+=1){
+
 	    if(data[i]!=null){
-		printable+=data[i];
+        if(nonNulls>=1){
+          printable+=",";
+        }
+        nonNulls+=1;
+        printable+=data[i];
+
 	    }
-	}
-	printable+="]";
-	return printable;
+
 
     }
+    printable+="]";
+    return printable;
+
+   }
+  /**
+    Returns the number of elements in this list.
+    @return the number of elements
+   */
+  public int size(){
+    int count=0;
+    for(int i=0;i<data.length;i+=1){
+      if(data[i]!=null){
+        count+=1;
+      }
+    }
+    return count;
+  }
+  /**
+     Appends the specified element to the end of this list. Returns true.
+     @param addend a String that is to be added to the end of the SuperArray
+     @return true if the given String is added to the end of the SuperArray
+
+   */
+  public boolean add(String addend){
+    size+=1;
+    String[] newdata=new String[size];
+    for(int i=0;i<data.length;i+=1){
+      newdata[i]=data[i];
+    }
+    newdata[size-1]=addend;
+    data=newdata;
+    return data[size-1]==addend;
+  }
+  /**
+    Removes all of the elements from this list.
+
+   */
+  public void clear(){
+    data=new String[size];
+  }
+  /**
+     Returns the element at the specified position in this list.
+  */
+  public String get(int index){
+    if(data[index]!=null){
+      return data[index];
+    }
+    else{
+      return "Array index out of bounds";
+    }
+  }
+
+  public String set(int index, String element){
+    String old="";
+    if(index<data.length){
+       old+=data[index];
+       data[index]=element;
+    }
+    else{
+      return "Array index out of bounds";
+    }
+    return old;
+  }
+  public boolean isEmpty(){
+    return size()==0;
+  }
+
 
 }
