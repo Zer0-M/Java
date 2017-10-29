2ce2bb6fdf643fe44764af0511ff8824401d3498
Mohammed Shafwat Jamil <msj2340@gmail.com>
2017-10-28 21:57:43 -0400

    finished adding methods

--- SuperArray/Driver.java
+++ SuperArray/Driver.java
@@ -20,9 +20,87 @@ public class Driver{
       //since we set a value lets get it
       System.out.println(suparr.get(8));//"hi"
       //now clear it
-      suparr.clear();
+      //suparr.clear();
       System.out.println(suparr);//[]
- 	
+      SuperArray arr = new SuperArray();
+
+      System.out.println(arr);//[]
+      for (int i=1;i<25;i+=2){
+        arr.add(i + "");
+      }
+      System.out.println(arr);//[1,3,5,7,9,11,13,15,17,19,21,23]
+
+      System.out.println(arr.get(2));//5
+      System.out.println(arr.get(3));//7
+      System.out.println(arr.get(23));//Error
+
+      System.out.println(arr.set(1,"pokemon"));//3
+      System.out.println(arr.set(5,"ninja"));//11
+      System.out.println(arr.set(-2,"hello"));//Error
+      System.out.println(arr);
+      //[1,pokemon,5,7,9,ninja,13,15,17,19,21,23]
+
+      arr.clear();
+      System.out.println(arr);//[]
+
+      for (int i=3;i<2000;i*=-7){
+        arr.add(i+"");
+        System.out.print(arr+"\n");
+        //[3],[3,-21],[3,-21,147],[3,-21,147,-1029]
+      }
+      arr.add("-21");
+      System.out.println(arr);//[3,-21,147,-1029,-21]
+      //Phase 3
+      System.out.println(arr.contains("-21"));
+      System.out.println(arr.contains("Waldo"));
+      System.out.println(suparr.contains("Eleven"));
+      System.out.println(suparr.contains("-21"));
+      System.out.println(arr.indexOf("-21"));
+      System.out.println(arr.indexOf("Waldo"));
+      System.out.println(suparr.indexOf("Eleven"));
+      System.out.println(suparr.indexOf("-21"));
+      suparr.add(1,"Mad Max");
+      arr.add(2,"72");
+      System.out.println(suparr);
+      System.out.println(arr);
+       System.out.println(arr.contains("20"));//false
+      System.out.println(arr.contains("147"));//true
+
+      System.out.println(arr.indexOf("-21"));//1
+      System.out.println(arr.indexOf("hello"));//-1
+      //System.out.println(arr.lastIndexOf("-21"));//4
+      //System.out.println(arr.lastIndexOf("hi"));//-1
+
+      arr.add(2,"mcDonalds");
+      System.out.println(arr);//[3,-21,mcDonalds,147,-1029,-21]
+
+      for (int i=0;i<7;i++){
+        arr.add(2,i+"");
+      }
+      System.out.println(arr);
+      //[3,-21,6,5,4,3,2,1,0,-21,mcDonalds,147,-1029,-21]
+
+      arr.add(999,"this isnt suppose to be here");//Error
+      System.out.println(arr);
+       //[3,-21,6,5,4,3,2,1,0,-21,mcDonalds,147,-1029,-21]
+
+
+      System.out.println(arr.remove(3));//5
+      System.out.println(arr);
+      //[3,-21,6,5,3,2,1,0,-21,mcDonalds,147,-1029,-21]
+
+      suparr.remove(6);
+      System.out.println(suparr);
+      
+      System.out.println(arr.remove(100));//Error
+      System.out.println(arr.remove("-21"));//true
+      System.out.println(arr);
+      //[3,-21,6,5,3,2,1,0,mcDonalds,147,-1029,-21]
+
+      System.out.println(arr.remove("105"));//false
+      System.out.println(arr);
+      //3,-21,6,5,3,2,1,0,mcDonalds,147,-1029,-21]
+      
     }
 
 }
--- SuperArray/SuperArray.java
+++ SuperArray/SuperArray.java
@@ -49,17 +49,16 @@ public class SuperArray{
      @return true if the given String is added to the end of the SuperArray
 
    */
-  public boolean add(String addend){
-      if(data[size-1]==null){
-	  data[size-1]=addend
+  public boolean add(String element){
+    int i=0;
+    while(data[i]!=null){
+        i+=1;
+        if(i==size-1){
+          resize();
+        }
       }
-      else{
-	  resize();
-	  
-      }
-
-
-    return data[size-1]==addend;
+    data[i]=element;
+    return data[i].equals(element);
   }
   /**
     Removes all of the elements from this list.
@@ -72,22 +71,27 @@ public class SuperArray{
      Returns the element at the specified position in this list.
   */
   public String get(int index){
-    if(data[index]!=null){
+    if(index>=0&&index<size){
       return data[index];
     }
     else{
-      return "no value found at index";
+      return "Invalid index";
     }
   }
 
   public String set(int index, String element){
     String old="";
-    if(index<data.length){
+    if(index<size&&index>=0){
+      if(data[index]==null){
+        old+="There was no element at this index";
+      }
+      else{
        old+=data[index];
+      }
        data[index]=element;
     }
     else{
-	return "Index out of bounds";
+      return "Index out of bounds";
     }
     return old;
   }
@@ -95,12 +99,87 @@ public class SuperArray{
     return size()==0;
   }
   private void resize(){
-    String[] newdata=new String[size*2];
+    size=size*2;
+    String[] newdata=new String[size];
     for(int i=0;i<data.length;i+=1){
       newdata[i]=data[i];
     }
     data=newdata;
 
+  }
+  public boolean contains(String element){
+    for(int i=0;i<data.length&&data[i]!=null;i+=1){
+      if(data[i].equals(element)){
+        return data[i].equals(element);
+      }
+
+    }
+    return false;
+  }
+
+  public int indexOf(String element){
+   for(int i=0;i<data.length&&data[i]!=null;i+=1){
+     if(data[i].equals(element)){
+        return i;
+      }
+
+    }
+    return -1;
+
+  }
+  public int lastIndexOf(String element){
+    int lastI=-1;
+    for(int i=0;i<data.length&&data[i]!=null;i+=1){
+      if(data[i].equals(element)){
+       lastI=i;
+      }
+
+    }
+    return lastI;
+  }
+  public void add(int index,String element){
+    if(index>=0&&index<size()){
+      resize();
+      int relativeIndex=0;
+      String nextEl=get(index+relativeIndex);
+      String nextNextEl=get(index+relativeIndex+1);
+      set(index,element);
+      while(nextEl!=null){
+        set(index+relativeIndex+1,nextEl);
+        nextEl=nextNextEl;
+        relativeIndex+=1;
+        nextNextEl=get(index+relativeIndex+1);
+      }
+    }
+    else{
+      System.out.println("Invalid index");
+    }
+
+  }
+  public String remove(int index){
+    if(index>=0&&index<size()){
+      int relativeIndex=1;
+      String removed=get(index);
+      set (index,get(index+relativeIndex));
+      while(get(index+relativeIndex)!=null){
+        relativeIndex+=1;
+        set(index+relativeIndex-1,get(index+relativeIndex));
+      }
+      return removed;
+    }
+    else{
+      return "Invalid index";
+    }
+  }
+  public boolean remove(String element){
+    boolean removed=false;
+    for(int i=0;i<size();i+=1){
+      if(get(i).equals(element)){
+        remove(i);
+        removed=true;
+      }
+    }
+    return removed;
   }
 
 }
