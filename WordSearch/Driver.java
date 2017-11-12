a62351f6c0dedf7a89e76596d44aae464ee38878
Mohammed Shafwat Jamil <msj2340@gmail.com>
2017-11-11 22:14:53 -0500

    word search now functional

--- WordSearch/Driver.java
+++ WordSearch/Driver.java
@@ -1,9 +1,8 @@
 public class Driver{
   public static void main(String [] args){
-    WordSearch word= new WordSearch(5,12);
+    WordSearch word= new WordSearch(20,20,"Computers.txt");
     //System.out.println(word);
-    word.clear();
-    word.addWordHorizontal("Clash",3,4);
+    /*word.addWordHorizontal("Clash",3,4);
     word.addWordHorizontal("Xenomorphon",1,2);
     word.addWordHorizontal("dalkfhlagjalgga",3,0);
     word.addWordHorizontal("Zoop",4,0);
@@ -11,9 +10,18 @@ public class Driver{
     word.addWordVertical("Zoro",0,11);
     word.addWordVertical("Donquixote",3,11);
     word.addWordVertical("Fly",0,2);
-    word.addWordDiagonal("DCA",2,3);
+    word.addWordDiagonal("DCA",2,3);*/
     System.out.println(word);
-
-
+    // WordSearch word0= new WordSearch(5,12);
+    /*
+     word0.addWord(3,4,"Clash",0,1);
+    word0.addWord(1,2,"Xenomorph",0,1);
+    word0.addWord(4,0,"Zoop",0,1);
+    word0.addWord(3,0,"adafgsgdhdhg",0,1);
+    word0.addWord(3,4,"Cl",1,0);
+    word0.addWord(3,11,"Donquixote",1,0);
+    word0.addWord(2,3,"DCA",1,1);
+    System.out.println(word0);
+    */
   }
 }
--- WordSearch/WordSearch.java
+++ WordSearch/WordSearch.java
@@ -1,5 +1,14 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class WordSearch{
     private char[][]data;
+    private Random randgen;
+    private ArrayList<String>wordsToAdd;
+    private ArrayList<String>wordsAdded;
 
     /**Initialize the grid to the size specified
 
@@ -9,7 +18,65 @@ public class WordSearch{
      */
     public WordSearch(int rows,int cols){
       data= new char[rows][cols];
+      clear();
     }
+    public WordSearch( int rows, int cols, String fileName){
+      data= new char[rows][cols];
+      clear();
+      wordsToAdd=new ArrayList<String>();
+      try{
+        File f = new File(fileName);//can combine
+        Scanner in = new Scanner(f);//into one line
+
+        while(in.hasNext()){
+          String word = in.next();
+          wordsToAdd.add(word);
+
+        }
+
+      }catch(FileNotFoundException e){
+        System.out.println("File not found: " + fileName);
+        //e.printStackTrace();
+        System.exit(1);
+      }
+      wordsAdded=new ArrayList<String>();
+      randgen=new Random();
+      addAllWords();
+      fillGrid();
+      System.out.println(wordsAdded);
+
+    }
+  public WordSearch( int rows, int cols, String fileName, int seed){
+      data= new char[rows][cols];
+      clear();
+      wordsToAdd=new ArrayList<String>();
+      try{
+        File f = new File(fileName);//can combine
+        Scanner in = new Scanner(f);//into one line
+
+        while(in.hasNext()){
+          String word = in.next();
+          wordsToAdd.add(word);
+
+        }
+
+      }catch(FileNotFoundException e){
+        System.out.println("File not found: " + fileName);
+        //e.printStackTrace();
+        System.exit(1);
+      }
+
+      wordsAdded=new ArrayList<String>();
+      randgen=new Random(seed);
+      addAllWords();
+      fillGrid();
+      System.out.println(wordsAdded);
+
+    }
+
+  //public WordSearch( int rows, int cols, int fileName, int randSeed)
+
+
 
     /**Set all values in the WordSearch to underscores'_'*/
   public void clear(){
@@ -38,7 +105,57 @@ public class WordSearch{
       }
       return grid;
     }
+  private boolean addAllWords(){
+    for(int i=0;i<wordsToAdd.size();i++){
+      String word=wordsToAdd.get(i);
+      int colInc=0;
+      int rowInc=0;
+      while(colInc==0&&rowInc==0){
+        colInc=randgen.nextInt(3)-1;
+        rowInc=randgen.nextInt(3)-1;
+      }
+      int row=randgen.nextInt(data[0].length);
+      int col=randgen.nextInt(data.length);
+      boolean added=addWord(row,col,word,rowInc,colInc);
+      if(added){
+        wordsAdded.add(word);
+      }
+    }
+    return true;
+  }
+  private boolean fillGrid(){
+    for(int row=0;row<data.length;row++){
+      for(int col=0;col<data[row].length;col++){
+        if(data[row][col]=='_'){
+          data[row][col]=(char) (randgen.nextInt(26)+65);
+        }
+      }
+    }
+    return true;
 
+  }
+    private boolean addWord( int row, int col, String word, int rowIncrement, int colIncrement){
+       try{
+             int testRow=row;
+             int testCol=col;
+             for(int ind=0;ind<word.length();ind++){
+               if(data[testRow][testCol]!='_'&&data[testRow][testCol]!=word.charAt(ind)){
+                 return false;
+               }
+               testRow+=rowIncrement;
+               testCol+=colIncrement;
+             }
+           }
+           catch(ArrayIndexOutOfBoundsException e){
+             return false;
+       }
+      for(int i=0;i<word.length();i++){
+           data[row][col]=word.charAt(i);
+           row+=rowIncrement;
+           col+=colIncrement;
+         }
+         return true;
+  }
 
 
     /**Attempts to add a given word to the specified position of the WordGrid.
